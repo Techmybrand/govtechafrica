69c1276c07ac7080161334f28afafabf81562bd9
Dominic <dominicezeifejafor@gmail.com>
2025-08-03 18:50:40 +0100

    404 page error message fix

--- components/404/NotFound.tsx
+++ components/404/NotFound.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import styles from './NotFound.module.scss';
 import Image from 'next/image';
+import styles from './NotFound.module.scss';
 
 const NotFound = () => {
   return (
     <div className={styles.not_found_container}>
       <div className={styles.details}>
         <p>404 error</p>
-        <h1>Under maintenance</h1>
+        {/* <h1>Under maintenance</h1> */}
         <h3>
           {`Sorry, the page you are looking for doesn't exist or has been moved. 
           Try searching our site:`}
