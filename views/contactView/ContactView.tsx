c28ea7bc90b828db842714056243939de2589a66
Dominic <dominicezeifejafor@gmail.com>
2026-01-12 08:47:57 +0100

    Removing discrepancies from the various sections and removed partnership page

--- views/contactView/ContactView.tsx
+++ views/contactView/ContactView.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import styles from "./ContactView.module.scss";
 import { Button, InputField, TextArea } from "@/shared";
+import styles from "./ContactView.module.scss";
 
 const ContactView = () => {
 	return (
