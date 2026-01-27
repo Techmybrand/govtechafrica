d55c26fdef6cd0ac1aaeedb9ed29be46d3f7e19f
Dominic <dominicezeifejafor@gmail.com>
2026-01-27 07:35:36 +0100

    pushed changes for articles images

--- views/blogView/BlogView.tsx
+++ views/blogView/BlogView.tsx
@@ -44,44 +44,27 @@ export default function BlogView() {
 				{!insightsList?.length ? (
 					<p>No posts available.</p>
 				) : (
-					// <div className={styles.row}>
-					// 	{insightsList.map((post, index: number) => (
-					// 		<div key={index} className={styles.card}>
-					// 			<div className={styles.grad}></div>
-					// 			<div className={styles.image}>
-					// 				<Image src={`https:${post?.thumbnail?.fields?.file?.url}`} fill 
-					// 					alt={`https:${post?.thumbnail?.fields?.description}`}
-					// 					sizes="100%"
-					// 				/>
-					// 			</div>
-					// 			<div className={styles.details}>
-					// 				<h5>{post.type}</h5>
-					// 				{/* <h3>{shortenTitle(post.title, 30)}</h3> */}
-					// 				<h3>{post.title}</h3>
-					// 				<p>{post?.description}</p>
-					// 				<Button
-					// 					className={styles.button}
-					// 					href={
-					// 						// post.tag === 'insight' ? 
-					// 						// `/insights/${formatURL(post.title)}` 
-					// 						// :
-					// 						`/insights/research/${formatURL(post.title)}`}
-					// 				>
-					// 					Explore
-					// 				</Button>
-					// 			</div>
-					// 		</div>
-					// 	))}
-					// </div>
 					<div className={styles.research_wrapper}>
-						{insightsList?.map((blog: BlogDetailsProps, index: number) =>
-							<ResearchCard key={index} title={blog?.title}
-								image={`https:${blog?.thumbnail?.fields?.file?.url}`}
-								alt={`https:${blog?.thumbnail?.fields?.description}`}
-								description={blog?.description}
-								btnText={blog?.type}
-							/>
-						)}
+						{insightsList?.map((blog: BlogDetailsProps, index: number) => {
+							const getImage = () => {
+								if (index === 0) {
+									return '/images/article_1.jpeg'
+								} else if (index === 1) {
+									return '/images/article_2.jpeg'
+								} else {
+									return `https:${blog?.thumbnail?.fields?.file?.url}`
+								}
+							}
+							const articleImage = getImage();
+							return (
+								<ResearchCard key={index} title={blog?.title}
+									image={articleImage}
+									alt={`https:${blog?.thumbnail?.fields?.description}`}
+									description={blog?.description}
+									btnText={blog?.type}
+								/>
+							)
+						})}
 					</div>
 				)}
 			</div>
