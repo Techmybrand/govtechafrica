63be1b29ad6fb5343764f036df81e064f40370a4
Dominic <dominicezeifejafor@gmail.com>
2026-02-28 22:04:16 +0100

    fix for undefined length

--- components/home/drivers/Drivers.tsx
+++ components/home/drivers/Drivers.tsx
@@ -32,7 +32,7 @@ const Drivers = () => {
 
 	const { scrollYProgress } = useScroll({
 		target: sectionRef,
-		offset: ["start end", "end end"]
+		offset: ["start end", "end 100%"]
 	});
 
 	const rawY = useTransform(scrollYProgress, [0, 0.2], [300, 0]);
